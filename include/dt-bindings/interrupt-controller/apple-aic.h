9325fe047a7664ea746b32c0acefe62a3c48635f
Hector Martin <marcan@marcan.st>
2021-02-04 23:01:56 +0900

    dt-bindings: interrupt-controller: Add DT bindings for apple-aic
    AIC is the Apple Interrupt Controller found on Apple ARM SoCs, such as
    the M1.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ include/dt-bindings/interrupt-controller/apple-aic.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+#ifndef _DT_BINDINGS_INTERRUPT_CONTROLLER_APPLE_AIC_H
+#define _DT_BINDINGS_INTERRUPT_CONTROLLER_APPLE_AIC_H
+
+#include <dt-bindings/interrupt-controller/irq.h>
+
+#define AIC_IRQ 0
+#define AIC_FIQ 1
+#define AIC_IPI 2
+
+#define AIC_TMR_PHYS 0
+#define AIC_TMR_VIRT 1
+
+#endif
